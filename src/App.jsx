25c68de8d74cc8caa3db128e5bbd240cf13c79bf
ImJaeOne <dlawi0108@naver.com>
2025-02-26 12:14:32 +0900

    🎉 Add: Supabase 기본 연동 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { supabase } from './api/client';
+
 function App() {
+  /**
+   * 'test' 테이블의 데이터를 가져옴
+   * @async
+   * @function getData
+   * @returns {Promise<object[]>} 가져온 데이터 배열
+   */
+  const getData = async () => {
+    const { data } = await supabase.from('test').select('*');
+    console.log(data);
+  };
+
+  getData();
+
   return <div className="text-blue-600">Hot Six~</div>;
 }
 
