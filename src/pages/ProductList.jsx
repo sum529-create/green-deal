3c0abd11c4d525fa91f2e4e349ec654b908840b4
ImJaeOne <dlawi0108@naver.com>
2025-02-27 01:10:49 +0900

    ✨ Update : ProductList basic UI #1

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import KakaoMap from '../components/KakaoMap/KakaoMap';
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className='flex w-full'>
+      <div className='w-[25%] bg-accent'>
+        검색
+      </div>
+      <div className='w-[75%]'>
+        <span className='text-2xl'>지금 우리 동네 인기 매물 TOP 20</span>
+        <KakaoMap width={'916px '} height={'640px'} />
+      </div>
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
