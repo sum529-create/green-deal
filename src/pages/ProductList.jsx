57e7a8ed1698a41ab7db656bc210a22d9e6fc427
ImJaeOne <dlawi0108@naver.com>
2025-02-27 11:06:32 +0900

    🎨 Style: Tailwind 설정 확장 및 기본 스타일 추가

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,8 +4,8 @@ import KakaoMap from '../components/KakaoMap/KakaoMap';
 const ProductList = () => {
   return (
     <div className="flex flex-col w-full h-screen md:flex-row">
-      <div className="w-full p-4 md:w-1/4 bg-accent">
-        <p className="text-lg font-bold">검색</p>
+      <div className="w-full p-4 md:w-1/4 bg-mint">
+        <p className="font-bold text-title-lg">검색</p>
       </div>
       <div className="flex flex-col w-full md:w-3/4">
         <span className="p-4 text-2xl">지금 우리 동네 인기 매물 TOP 20</span>
