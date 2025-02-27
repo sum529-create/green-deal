44d0fccab888c72d994da89d0c13de0469c2ecf1
DnJ0408 <oyj921205@gmail.com>
2025-02-27 17:08:02 +0900

    ✨ Update: Function Custom Overlay #6

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { CustomOverlayMap, Map, MapMarker } from 'react-kakao-maps-sdk';
 import { getUserLocation } from '../../utils/getUserLocation';
-import { DUMMY_LISTS } from '../../constants/DUMMY_LISTS';
 
-const KakaoMap = ({ level, mode }) => {
+const KakaoMap = ({ level, mode, productList }) => {
   const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 });
+  const [productInfo, setProductInfo] = useState(null);
 
   useEffect(() => {
     getUserLocation()
       .then((coords) => setLocation(coords))
       .catch((error) => console.error(error));
   }, []);
+
+  const handleClickProduct = (product) => {
+    setProductInfo(product);
+  };
 
   return (
     <div className="w-full h-full">
@@ -19,18 +23,30 @@ const KakaoMap = ({ level, mode }) => {
         level={level}
         style={{ width: '100%', height: '100%' }}
       >
-        {mode === 'productList' && (
-          <div>
-            {DUMMY_LISTS.map((product) => {
-              return (
-                <MapMarker
-                  key={product.id}
-                  position={product.location}
-                ></MapMarker>
-              );
-            })}
-          </div>
-        )}
+        {mode === 'productList' &&
+          productList.map((product) => (
+            <div key={product.id}>
+              <MapMarker
+                position={product.location}
+                onClick={() => handleClickProduct(product)}
+              />
+
+              {productInfo && productInfo.id === product.id && (
+                <CustomOverlayMap position={product.location} yAnchor={1.7}>
+                  <div
+                    style={{
+                      padding: '42px',
+                      backgroundColor: '#fff',
+                      border: '1px solid black',
+                      color: '#000',
+                    }}
+                  >
+                    {product.product_description}
+                  </div>
+                </CustomOverlayMap>
+              )}
+            </div>
+          ))}
       </Map>
     </div>
   );
