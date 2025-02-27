c0e2d8860222d99a25d31723e5832cd0344f405f
ImJaeOne <dlawi0108@naver.com>
2025-02-27 14:30:29 +0900

    ✨ Add: SearchList 컴포넌트 생성 #6

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import KakaoMap from '../components/KakaoMap/KakaoMap';
 import SearchBar from '../components/ProductList/SearchBar';
 import { useState } from 'react';
+import SearchList from '../components/ProductList/SearchList';
 
 const ProductList = () => {
   const [search, setSearch] = useState('');
 
   console.log(search);
-  
+
   return (
     <div className="flex flex-col w-full h-screen md:flex-row">
       <div className="w-full p-4 bg-white md:w-1/4">
         <SearchBar setSearch={setSearch} />
+        <SearchList />
       </div>
       <div className="flex flex-col w-full md:w-3/4">
         <span className="p-4 text-2xl">지금 우리 동네 인기 매물 TOP 20</span>
