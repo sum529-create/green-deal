a9780cfd4d7e8844c8aea09c8685b6c190298f60
ImJaeOne <dlawi0108@naver.com>
2025-02-27 22:29:35 +0900

    ✨ Update: 리스트 선택 시 해당 위치를 중심으로 설정 #4

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -5,12 +5,16 @@ import { Link } from 'react-router-dom';
 import { useRef } from 'react';
 
 const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
-  const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 });
+  const [location, setLocation] = useState({ lat: null, lng: null }); // 유저의 중심 위치를 위한 상태
   const [productInfo, setProductInfo] = useState(null);
+  const [center, setCenter] = useState(location); // 지도의 중심 위치를 위한 상태
 
   useEffect(() => {
     getUserLocation()
-      .then((coords) => setLocation({ lat: 37.5568, lng: 126.9237 })) // 더미 데이터 확인을 위한 위도 경도 설정입니다.
+      .then((coords) => {
+        setLocation({ lat: 37.5568, lng: 126.9237 });
+        setCenter({ lat: 37.5568, lng: 126.9237 });
+      }) // 더미 데이터 확인을 위한 위도 경도 설정입니다. 추후에 coords로 변경 예정
       .catch((error) => console.error(error));
   }, []);
 
@@ -19,6 +23,9 @@ const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
       const foundProduct = productList.find(
         (product) => product.id === selectedProduct,
       );
+      if (foundProduct) {
+        setCenter(foundProduct.location); // 지도 중심을 해당 위치로 이동
+      }
       setProductInfo(foundProduct || null);
     }
   }, [selectedProduct, productList]);
@@ -34,7 +41,7 @@ const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
   return (
     <div className="w-full h-full">
       <Map
-        center={location}
+        center={center}
         level={level}
         style={{ width: '100%', height: '100%' }}
         onClick={handleClickMap}
@@ -68,7 +75,8 @@ const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
                   yAnchor={1.4}
                   clickable={true}
                 >
-                  <Link to={`/product/detail?id=${product.id}`}
+                  <Link
+                    to={`/product/detail?id=${product.id}`}
                     style={{
                       pointerEvents: 'auto',
                       display: 'flex',
@@ -85,19 +93,19 @@ const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
                       pointerEvents: 'auto',
                     }}
                   >
-                      <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
-                        {product.name}
-                      </div>
-                      <div style={{ color: '#666', marginBottom: '5px' }}>
-                        {Number(product.price).toLocaleString()}원
-                      </div>
-                      <div style={{ fontSize: '12px', color: '#888' }}>
-                        {product.users.name}{' '}
-                        {new Date(product.createdAt)
-                          .toISOString()
-                          .slice(2, 10)
-                          .replace(/-/g, '.')}
-                      </div>
+                    <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                      {product.name}
+                    </div>
+                    <div style={{ color: '#666', marginBottom: '5px' }}>
+                      {Number(product.price).toLocaleString()}원
+                    </div>
+                    <div style={{ fontSize: '12px', color: '#888' }}>
+                      {product.users.name}{' '}
+                      {new Date(product.createdAt)
+                        .toISOString()
+                        .slice(2, 10)
+                        .replace(/-/g, '.')}
+                    </div>
                   </Link>
                 </CustomOverlayMap>
               )}
