665de1163494f4edc648d45a0bcd9aee7bc22fdd
DnJ0408 <oyj921205@gmail.com>
2025-02-27 14:36:32 +0900

    ✨ Update : My Location Marker #6

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { CustomOverlayMap, Map, MapMarker } from 'react-kakao-maps-sdk';
 import { getUserLocation } from '../../utils/getUserLocation';
 
-const KakaoMap = ({ level }) => {
+const KakaoMap = ({ level, mode }) => {
   const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 });
 
   useEffect(() => {
@@ -12,11 +12,21 @@ const KakaoMap = ({ level }) => {
   }, []);
 
   return (
-    <Map
-      center={location}
-      level={level}
-      style={{ width: '100%', height: '100%' }}
-    />
+    <div className="w-full h-full">
+      <Map
+        center={location}
+        level={level}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {mode === 'productList' && (
+          <div>
+            <MapMarker position={location}>
+              <div>아이폰 팔아요</div>
+            </MapMarker>
+          </div>
+        )}
+      </Map>
+    </div>
   );
 };
 
