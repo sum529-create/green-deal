ac050898763303054f1a67163ea64409db25c7a8
ImJaeOne <dlawi0108@naver.com>
2025-02-27 09:12:24 +0900

    ✨ Update: 현재 위치를 중심으로 지도 표시 기능 추가 #1

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
+import { getUserLocation } from '../../utils/getUserLocation';
 
 const KakaoMap = ({ width, height }) => {
+  const [location, setLocation] = useState({ lat: 33.450701, lng: 126.570667 });
+
+  useEffect(() => {
+    getUserLocation()
+      .then((coords) => setLocation(coords))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <Map
-      center={{ lat: 33.450701, lng: 126.570667 }}
-      style={{ width: `${width}`, height: `${height}` }}
+      center={location}
+      className="w-full h-full"
+      style={{ width, height }}
       level={3}
     />
   );
