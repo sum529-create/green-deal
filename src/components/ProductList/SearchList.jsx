fa40b709fa1b6cdfc3386ba5ec64b39aa35b8961
ImJaeOne <dlawi0108@naver.com>
2025-02-27 15:11:11 +0900

    ✨ Update: 검색된 결과를 SearchList에 반영 #6

--- src/components/ProductList/SearchList.jsx
+++ src/components/ProductList/SearchList.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const SearchList = () => {
-  return <div>SearchList</div>;
+const SearchList = ({ filteredProducts }) => {
+  return (
+    <div className="flex flex-col bg-white">
+      {filteredProducts?.map((product) => {
+        return (
+          <div className="flex justify-between py-5 border-b-2 px-9 border-light-gray">
+            <div className="flex flex-col gap-2">
+              <span className="font-bold text-title-sm">
+                {product.product_name}
+              </span>
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-10 h-10 border-2 rounded-full border-light-gray bg-light-gray"></div>
+                <span className="text-text-lg">판매자 이름</span>
+              </div>
+            </div>
+            <img
+              src={product.product_img === '' ? 'https://placehold.co/150' : ''}
+              alt="사진 없음"
+              className="w-20 h-20"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SearchList;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,16 +4,96 @@ import SearchBar from '../components/ProductList/SearchBar';
 import { useState } from 'react';
 import SearchList from '../components/ProductList/SearchList';
 
+const productList = [
+  {
+    id: 1,
+    createdAt: '2024-02-27T12:34:56.789Z',
+    product_name: '아이폰11',
+    product_category: '디지털기기',
+    product_price: '10000',
+    product_quality: '최상',
+    product_refund: false,
+    product_location: { lat: 33.450701, lng: 126.570667 },
+    product_description: '싸다 싸! 신발보다 싸!',
+    product_img: '',
+    user_id: 1,
+    product_soldout: true,
+    updated_at: '',
+  },
+  {
+    id: 2,
+    createdAt: '2024-02-28T12:34:56.789Z',
+    product_name: '축구공',
+    product_category: '스포츠/레저',
+    product_price: '100000',
+    product_quality: '중상',
+    product_refund: false,
+    product_location: { lat: 33.250701, lng: 126.270667 },
+    product_description: '사용감 살짝 있습니다!',
+    product_img: '',
+    user_id: 2,
+    product_soldout: true,
+    updated_at: '2024-02-29T12:34:56.789Z',
+  },
+  {
+    id: 3,
+    createdAt: '2024-02-27T12:38:56.789Z',
+    product_name: '맥북 Air',
+    product_category: '디지털기기',
+    product_price: '1000000',
+    product_quality: '최상',
+    product_refund: false,
+    product_location: { lat: 33.550701, lng: 126.670667 },
+    product_description: '개봉만 했습니다.',
+    product_img: '',
+    user_id: 1,
+    product_soldout: true,
+    updated_at: '',
+  },
+  {
+    id: 4,
+    createdAt: '2024-02-27T12:34:56.789Z',
+    product_name: '아이폰11',
+    product_category: '디지털기기',
+    product_price: '10000',
+    product_quality: '최상',
+    product_refund: false,
+    product_location: { lat: 33.450701, lng: 126.570667 },
+    product_description: '싸다 싸! 신발보다 싸!',
+    product_img: '',
+    user_id: 1,
+    product_soldout: true,
+    updated_at: '',
+  },
+  {
+    id: 5,
+    createdAt: '2024-02-27T12:34:56.789Z',
+    product_name: '아이폰11',
+    product_category: '디지털기기',
+    product_price: '10000',
+    product_quality: '최상',
+    product_refund: false,
+    product_location: { lat: 33.450701, lng: 126.570667 },
+    product_description: '싸다 싸! 신발보다 싸!',
+    product_img: '',
+    user_id: 1,
+    product_soldout: true,
+    updated_at: '',
+  },
+];
+
 const ProductList = () => {
   const [search, setSearch] = useState('');
 
-  console.log(search);
+  const filteredProducts = productList.filter((product) =>
+    product.product_name.includes(search),
+  );
 
   return (
     <div className="flex flex-col w-full h-screen md:flex-row">
-      <div className="w-full p-4 bg-white md:w-1/4">
+      <div className="w-full mr-4 border-r-2 border-light-gray bg-white md:w-[360px]">
         <SearchBar setSearch={setSearch} />
-        <SearchList />
+        <SearchList filteredProducts={filteredProducts || []} />
       </div>
       <div className="flex flex-col w-full md:w-3/4">
         <span className="p-4 text-2xl">지금 우리 동네 인기 매물 TOP 20</span>
