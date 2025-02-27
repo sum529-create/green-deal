a0de0fdd3c3fa7df7865fb5e88a95cf7ad95cad2
ImJaeOne <dlawi0108@naver.com>
2025-02-27 19:20:53 +0900

    🎨 Style: 화면 높이를 넘어가는 물품 리스트 스크롤 처리 #4

--- src/components/productlist/SearchList.jsx
+++ src/components/productlist/SearchList.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 
-const SearchList = ({ filteredProducts }) => {
+const SearchList = ({
+  filteredProducts,
+  selectedProduct,
+  setSelectedProduct,
+}) => {
+  const handleClick = (id) => {
+    setSelectedProduct(id);
+  };
   return (
-    <div className="flex flex-col overflow-auto text-black bg-white border-t-2 border-light-gray h-[calc(100vh-122px)]">
+    <div className="flex flex-col overflow-auto text-black bg-white border-t-2 border-light-gray h-[calc(100vh-122px)] ">
       {filteredProducts?.map((product) => {
         return (
-          <div className="flex justify-between py-5 border-b-2 cursor-pointer group px-9 border-light-gray">
+          <div
+            className={`flex justify-between py-5 cursor-pointer group px-9 ${selectedProduct === product.id ? 'bg-light-gray' : 'border-b border-light-gray'}`}
+            onClick={() => handleClick(product.id)}
+          >
             <div className="flex flex-col gap-2">
-              <span className="font-bold text-title-sm group-hover:text-deep-mint">
+              <span
+                className={`font-bold text-title-sm ${selectedProduct === product.id ? 'text-light-mint' : 'group-hover:text-mint'}`}
+              >
                 {product.name}
               </span>
+
               <div className="flex items-center justify-center gap-2">
-                <div className="w-10 h-10 border-2 rounded-full border-light-gray bg-light-gray"></div>
-                <span className="text-text-lg">판매자 이름</span>
+                <img
+                  src={product.users.profile_img}
+                  className="w-10 h-10 rounded-full bg-deep-mint"
+                ></img>
+                <span className="text-text-lg">{product.users.name}</span>
               </div>
             </div>
             <img
-              src={product.img === '' ? 'https://placehold.co/150' : ''}
+              src={
+                product.img === '' ? 'https://placehold.co/150' : product.img
+              }
               alt="사진 없음"
-              className="w-20 h-20"
+              className="w-20 h-20 rounded-sm"
             />
           </div>
         );
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -15,10 +15,15 @@ const productList = [
     refund: false,
     location: { lat: 33.450701, lng: 126.570667 },
     description: '싸다 싸! 신발보다 싸!',
-    img: '',
+    img: 'https://alephksa.com/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-1A__WWEN_0853ab85-adc4-47fb-8955-43df09cca9f1.jpg?v=1688733593&width=2048',
     user_id: 1,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '임재원',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/2-25-11-73.png',
+    },
   },
   {
     id: 2,
@@ -30,10 +35,15 @@ const productList = [
     refund: false,
     location: { lat: 33.250701, lng: 126.270667 },
     description: '사용감 살짝 있습니다!',
-    img: '',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8aVBdXsDP6B4DJ9un1nLcIF-jwa3n-fjUeQ&s',
     user_id: 2,
     soldout: true,
     updated_at: '2024-02-29T12:34:56.789Z',
+    users: {
+      name: '오영진',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/10-539-44-430.png',
+    },
   },
   {
     id: 3,
@@ -45,10 +55,15 @@ const productList = [
     refund: false,
     location: { lat: 33.550701, lng: 126.670667 },
     description: '개봉만 했습니다.',
-    img: '',
-    user_id: 1,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVi1KsUu2PdmxovPkDmUbXtykLZwkg4RavvA&s',
+    user_id: 3,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '김지윤',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/7-361-52-630.png',
+    },
   },
   {
     id: 4,
@@ -58,12 +73,17 @@ const productList = [
     price: '10000',
     quality: '최상',
     efund: false,
-    location: { lat: 33.450701, lng: 126.570667 },
+    location: { lat: 33.450701, lng: 126.570666 },
     description: '싸다 싸! 신발보다 싸!',
-    img: '',
-    user_id: 1,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVi1KsUu2PdmxovPkDmUbXtykLZwkg4RavvA&s',
+    user_id: 4,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '최수진',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/8-36-63-636.png',
+    },
   },
   {
     id: 5,
@@ -75,10 +95,15 @@ const productList = [
     refund: false,
     location: { lat: 33.450701, lng: 126.570667 },
     description: '싸다 싸! 신발보다 싸!',
-    img: '',
-    user_id: 1,
+    img: 'https://alephksa.com/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-1A__WWEN_0853ab85-adc4-47fb-8955-43df09cca9f1.jpg?v=1688733593&width=2048',
+    user_id: 5,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '장미경',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/10-368-4-573.png',
+    },
   },
   {
     id: 6,
@@ -88,12 +113,17 @@ const productList = [
     price: '10000',
     quality: '최상',
     refund: false,
-    location: { lat: 33.450701, lng: 126.570667 },
+    location: { lat: 33.450701, lng: 126.576667 },
     description: '싸다 싸! 신발보다 싸!',
-    img: '',
+    img: 'https://alephksa.com/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-1A__WWEN_0853ab85-adc4-47fb-8955-43df09cca9f1.jpg?v=1688733593&width=2048',
     user_id: 1,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '최수진',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/2-25-11-73.png',
+    },
   },
   {
     id: 7,
@@ -103,17 +133,23 @@ const productList = [
     price: '10000',
     quality: '최상',
     refund: false,
-    location: { lat: 33.450701, lng: 126.570667 },
+    location: { lat: 33.450301, lng: 126.570667 },
     description: '싸다 싸! 신발보다 싸!',
-    img: '',
+    img: 'https://alephksa.com/cdn/shop/files/iPhone_14_Blue_PDP_Image_Position-1A__WWEN_0853ab85-adc4-47fb-8955-43df09cca9f1.jpg?v=1688733593&width=2048',
     user_id: 1,
     soldout: true,
     updated_at: '',
+    users: {
+      name: '노수민',
+      profile_img:
+        'https://cdn-static.zep.us/static/assets/baked-avartar-images/10-35-3-253.png',
+    },
   },
 ];
 
 const ProductList = () => {
   const [search, setSearch] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const filteredProducts = productList.filter((product) =>
     product.name.includes(search),
@@ -121,9 +157,14 @@ const ProductList = () => {
 
   return (
     <div className="flex flex-col w-full h-screen overflow-hidden md:flex-row">
-      <div className="w-full mr-4 border-r-2 border-light-gray h-full bg-white md:w-[360px]">
+      <div className="w-full mr-4 border-r border-light-gray h-full bg-white md:w-[360px]">
         <SearchBar setSearch={setSearch} />
-        <SearchList filteredProducts={filteredProducts || []} />
+        <SearchList
+          filteredProducts={filteredProducts || []}
+          selectedProduct={selectedProduct}
+          setSelectedProduct={setSelectedProduct}
+          setSearch={setSelectedProduct}
+        />
       </div>
       <div className="flex flex-col w-full md:w-3/4">
         <span className="p-4 text-2xl">지금 우리 동네 인기 매물 TOP 20</span>
