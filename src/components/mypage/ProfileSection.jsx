5e98432a89c39223f8b2c6af15e9b848d781767e
verdantgreeny <verdantgreeny@gmail.com>
2025-02-27 19:51:04 +0900

    🍻 Test: supabase 퍼블릭에 있는 유저 데이터 가져와 출력 테스트

--- src/components/mypage/ProfileSection.jsx
+++ src/components/mypage/ProfileSection.jsx
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
 import Button from '../common/Button';
+import { supabase } from '../../api/client';
+import { useEffect } from 'react';
 
 const ProfileSection = () => {
   const [nickname, setNickname] = useState('테스트닉네임');
   const [isUpdating, setIsUpdating] = useState(false);
+  const [testUser, setTestUser] = useState(null);
+
+  const getUserData = async () => {
+    const { data, error } = await supabase.from('users').select('*');
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log('User data:', data);
+    }
+    setTestUser(data[0]);
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  console.log('test user:', testUser);
+
+  if (!testUser) {
+    return <div>Loading...</div>;
+  }
 
   const handleToggleClick = () => {
     setIsUpdating((prev) => !prev);
@@ -12,6 +34,7 @@ const ProfileSection = () => {
   const handleNicknameChange = (e) => {
     setNickname(e.target.value);
   };
+
   return (
     <div className="flex flex-col items-center justify-center gap-4 w-[300px] h-[458px] px-10 py-[30px] bg-white">
       <Button
@@ -23,7 +46,7 @@ const ProfileSection = () => {
         이미지 선택
       </Button>
       <img
-        src={null}
+        src={testUser.profile_img || null}
         alt="프로필 이미지"
         className="object-cover w-[130px] h-[130px] bg-light-gray rounded-full"
       />
@@ -35,9 +58,9 @@ const ProfileSection = () => {
           className="w-[210px] h-[32px] border border-dark rounded-md text-center"
         />
       ) : (
-        <p className="text-title-sm">{nickname}</p>
+        <p className="text-title-sm">{testUser.name}</p>
       )}
-      <p className="text-title-sm">email@example.com</p>
+      <p className="text-title-sm">{testUser.email}</p>
       <Button
         type="button"
         variant="primary"
