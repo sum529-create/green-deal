0d1297219d223aabf66756c525f5caf9b274f2ac
jinew <jiyunk@naver.com>
2025-02-28 22:26:43 +0900

    ♻ refactor : store 리팩토링 #25
    store에서는 상태 관리 로직만 처리할 수 있게 기존 api 통신 관련 액션 함수를 정리했습니다.

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -6,32 +6,18 @@ const initialState = {
   isLogin: false,
 };
 
-const useUserStore = create((set) => ({
-  ...initialState,
-  userLogin: () => {
-    try {
-      const { error } = supabase.auth.onAuthStateChange((_, session) => {
-        if (session) {
-          set({ user: session.user, isLogin: true });
-        } else {
-          set(initialState);
-        }
-
-        if (error) throw error;
-      });
-    } catch (error) {
-      console.log('로그인 에러', error);
-    }
-  },
-  userLogOut: async () => {
-    try {
-      const { error } = await supabase.auth.signOut();
-      if (error) throw error;
+const useUserStore = create((set) => {
+  supabase.auth.onAuthStateChange((_, session) => {
+    if (session) {
+      set({ user: session.user, isLogin: true });
+    } else {
       set(initialState);
-    } catch (error) {
-      console.log('로그아웃 에러', error);
     }
-  },
-}));
+  });
+
+  return {
+    ...initialState,
+  };
+});
 
 export default useUserStore;
