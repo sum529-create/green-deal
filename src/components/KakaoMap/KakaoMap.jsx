7bd2bfd80af1c55cc5ac43409b074d91e5607640
DnJ0408 <oyj921205@gmail.com>
2025-02-28 14:45:59 +0900

    🩹 Fix: My location기능 productList 모드 내부로 이동 #24

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -46,71 +46,65 @@ const KakaoMap = ({ level, mode, productList, selectedProduct }) => {
         style={{ width: '100%', height: '100%' }}
         onClick={handleClickMap}
       >
-        <MapMarker
-          position={location} // ip 기반으로 현재 내 위치 지정
-          image={{
-            src: 'https://cdn-static.zep.us/static/assets/baked-avartar-images/10-35-3-253.png', // 프로필 사진 들어갈 예정
-            size: {
-              width: 39,
-              height: 39,
-            },
-            options: {
-              offset: {
-                x: 27,
-                y: 69,
-              },
-            },
-          }}
-        ></MapMarker>
-        {mode === 'productList' &&
-          productList.map((product) => (
-            <div key={product.id}>
-              <MapMarker
-                position={product.location}
-                onClick={() => handleClickProduct(product)}
-              />
-              {productInfo && productInfo.id === product.id && (
-                <CustomOverlayMap
+        {mode === 'productList' && (
+          <>
+            <MapMarker
+              position={location}
+              image={{
+                src: 'null',
+                size: { width: 39, height: 39 },
+                options: { offset: { x: 27, y: 69 } },
+              }}
+            />
+            {productList.map((product) => (
+              <div key={product.id}>
+                <MapMarker
                   position={product.location}
-                  yAnchor={1.4}
-                  clickable={true}
-                >
-                  <Link
-                    to={`/product/detail?id=${product.id}`}
-                    style={{
-                      pointerEvents: 'auto',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      padding: '20px',
-                      backgroundColor: '#fff',
-                      border: '2px solid rgb(85 204 201)',
-                      borderRadius: '15px',
-                      color: '#000',
-                      textAlign: 'center',
-                      minWidth: '120px',
-                      pointerEvents: 'auto',
-                    }}
+                  onClick={() => handleClickProduct(product)}
+                />
+                {productInfo && productInfo.id === product.id && (
+                  <CustomOverlayMap
+                    position={product.location}
+                    yAnchor={1.4}
+                    clickable={true}
                   >
-                    <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
-                      {product.name}
-                    </div>
-                    <div style={{ color: '#666', marginBottom: '5px' }}>
-                      {Number(product.price).toLocaleString()}원
-                    </div>
-                    <div style={{ fontSize: '12px', color: '#888' }}>
-                      {product.users.name}{' '}
-                      {new Date(product.createdAt)
-                        .toISOString()
-                        .slice(2, 10)
-                        .replace(/-/g, '.')}
-                    </div>
-                  </Link>
-                </CustomOverlayMap>
-              )}
-            </div>
-          ))}
+                    <Link
+                      to={`/product/detail?id=${product.id}`}
+                      style={{
+                        pointerEvents: 'auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        padding: '20px',
+                        backgroundColor: '#fff',
+                        border: '2px solid rgb(85 204 201)',
+                        borderRadius: '15px',
+                        color: '#000',
+                        textAlign: 'center',
+                        minWidth: '120px',
+                      }}
+                    >
+                      <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+                        {product.name}
+                      </div>
+                      <div style={{ color: '#666', marginBottom: '5px' }}>
+                        {Number(product.price).toLocaleString()}원
+                      </div>
+                      <div style={{ fontSize: '12px', color: '#888' }}>
+                        {product.users.name}{' '}
+                        {new Date(product.createdAt)
+                          .toISOString()
+                          .slice(2, 10)
+                          .replace(/-/g, '.')}
+                      </div>
+                    </Link>
+                  </CustomOverlayMap>
+                )}
+              </div>
+            ))}
+          </>
+        )}
       </Map>
     </div>
   );
