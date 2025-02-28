ad0e8726aa17d969e58870fa97918e1663d634e5
DnJ0408 <oyj921205@gmail.com>
2025-02-28 18:00:11 +0900

    🧹 Cleanup: 주석추가 handleClickProduct #24

--- src/components/KakaoMap/MapProductMarker.jsx
+++ src/components/KakaoMap/MapProductMarker.jsx
@@ -2,6 +2,7 @@ import { MapMarker } from 'react-kakao-maps-sdk';
 import MapProductModal from './MapProductModal';
 
 const MapProductMarker = ({ productList, productInfo, setProductInfo }) => {
+  // 특정 마커 클릭 시 그 제품의 정보를 모달로 전달
   const handleClickProduct = (product) => {
     setProductInfo(product);
   };
