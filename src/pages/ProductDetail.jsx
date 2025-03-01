90b0e9f43bf1b2a46872d050e40823cab54019dd
lyra <mk.lyra.j@gmail.com>
2025-03-02 01:51:29 +0900

    ♻️ Refactor : 컴포넌트로 코드 분리 #28

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { FaRegStar } from 'react-icons/fa';
 import Comments from '../components/comments/Comments';
 import { useParams } from 'react-router-dom';
 import useUserStore from '../store/userStore';
 import Button from '../components/common/Button';
 import { supabase } from '../api/client';
+import ProductHeader from '../components/productdetail/ProductHeader';
+import SellerInfo from '../components/productdetail/SellerInfo';
+import ProductInfo from '../components/productdetail/ProductInfo';
+import ProductDescription from '../components/productdetail/ProductDescription';
 
 const ProductDetail = () => {
   // State
@@ -101,69 +104,26 @@ const ProductDetail = () => {
 
         {/* 상품 정보 */}
         <div className="w-[630]">
-          <div className="flex flex-col pace-y-6">
-            {/* 상품명, 판매여부 & 찜하기 */}
-            <div className="flex justify-between p-4">
-              <div className="flex items-center">
-                <h2 className="text-2xl font-bold">{product.name}</h2>
-                <span
-                  className={`flex items-center justify-center mx-4 px-3 text-caption min-w-[80px] rounded-full ${product.soldout ? 'bg-gray text-deep-gray' : 'bg-deep-mint text-white'}`}
-                >
-                  {product.soldout ? '판매 완료' : '판매중'}
-                </span>
-              </div>
-              <FaRegStar className="text-2xl text-gray-500 cursor-pointer hover:text-yellow-400" />
-            </div>
-
-            {/* 상품 가격*/}
-            <p className="pl-4 text-lg font-semibold text-deep-mint">
-              {Number(product.price).toLocaleString()}원
-            </p>
-            <div className="flex items-center gap-4"></div>
-          </div>
+          {/* 상품 정보 헤더 컴포넌트 */}
+          <ProductHeader product={product} />
 
           <hr className="border-t-1 border-light-gray" />
 
-          {/* 판매자 정보 */}
-          <div className="flex items-center gap-3">
-            <img
-              src={seller?.profile_img}
-              alt={seller?.name}
-              className="w-10 h-10 bg-gray-300 rounded-full"
-            />
-            <span className="text-gray-700">{seller?.name}</span>
-          </div>
+          {/* 판매자 정보 컴포넌트 */}
+          <SellerInfo seller={seller} />
 
           <hr className="border-t-1 border-light-gray" />
 
-          {/* 상품 상세 정보 */}
           <div className="flex flex-col w-[630px]">
-            <div className="p-4 space-y-2 bg-gray-100 rounded-lg">
-              <h3 className="text-lg font-semibold text-gray-700">상품정보</h3>
-              <p className="text-sm">
-                <strong>상태 :</strong> {product.quality}
-              </p>
-              <p className="text-sm">
-                <strong>교환 :</strong> {product.refund ? '가능' : '불가능'}
-              </p>
-              <p className="text-sm">
-                <strong>카테고리:</strong> {product.category}
-              </p>
-            </div>
+            {/* 상품 상세 정보 컴포넌트 */}
+            <ProductInfo product={product} />
 
             <hr className="border-t-1 border-light-gray" />
 
-            {/* 상품 설명 */}
-            <div>
-              <div>
-                <h3 className="text-lg font-semibold text-gray-700">
-                  상품 설명
-                </h3>
-                <p className="text-gray-600">{product.description}</p>
-              </div>
-            </div>
+            {/* 상품 설명 컴포넌트 */}
+            <ProductDescription product={product} />
 
-            {/* 지도 */}
+            {/* 지도 영역 */}
             <div className="flex flex-col items-center p-4 bg-gray-200 rounded-lg">
               <div className="w-[400px] h-[250px] rounded-lg bg-gray">
                 KKO MAP
