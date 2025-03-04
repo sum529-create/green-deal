41eda61ccdd2c883a1aaf2a24fe17d09520dc38c
lyra <mk.lyra.j@gmail.com>
2025-03-04 14:54:58 +0900

    ♻️ Refactor : 찜 추가 하기 Optimistic Update #70

--- src/hooks/useWish.js
+++ src/hooks/useWish.js
@@ -21,8 +21,23 @@ export const useGetWishes = (productId) => {
   });
 };
 
+// /**
+//  * useAddWish
+//  * @description 특정 상품을 찜하는 훅
+//  * @param {number} productId - 찜할 상품 ID
+//  * @returns {object} - 찜 추가 Mutation 객체
+//  */
+// export const useAddWish = (productId) => {
+//   const queryClient = useQueryClient();
+//   return useMutation({
+//     mutationFn: ({ userId }) => addWish({ productId, userId }),
+//     onSuccess: () => {
+//       queryClient.invalidateQueries([QUERY_KEYS.WISH.LIST, productId]);
+//     },
+//   });
+// };
 /**
- * useAddWish
+ * useAddWish Optimistic-Update
  * @description 특정 상품을 찜하는 훅
  * @param {number} productId - 찜할 상품 ID
  * @returns {object} - 찜 추가 Mutation 객체
@@ -30,8 +45,33 @@ export const useGetWishes = (productId) => {
 export const useAddWish = (productId) => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({ userId }) => addWish({ productId, userId }),
-    onSuccess: () => {
+    mutationFn: ({ productId, userId }) => addWish({ productId, userId }),
+    onMutate: async ({ productId, userId }) => {
+      // 기존 쿼리 취소
+      await queryClient.cancelQueries([QUERY_KEYS.WISH.LIST, productId]);
+      // 현재 상태 저장 (Snapshot)
+      const prevWish = queryClient.getQueryData([
+        QUERY_KEYS.WISH.LIST,
+        productId,
+      ]);
+      // UI 즉시 반영
+      queryClient.setQueryData([QUERY_KEYS.WISH.LIST, productId], (old) => [
+        ...(old || []), // 기존 찜 목록 유지
+        { product_id: productId, user_id: userId }, // 새로운 찜 데이터
+      ]);
+      return { prevWish };
+    },
+    // 에러시 롤백
+    onError: (err, newWish, context) => {
+      if (context.prevWish) {
+        queryClient.setQueryData(
+          [QUERY_KEYS.WISH.LIST, productId],
+          context.prevWish,
+        );
+      }
+    },
+    // 최종 서버 데이터 동기화
+    onSettled: () => {
       queryClient.invalidateQueries([QUERY_KEYS.WISH.LIST, productId]);
     },
   });
