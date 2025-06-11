67131f5be1dcbf90f2b939386f8a626bab26e2b7
노수민 <nosumin29@gmail.com>
2025-06-11 13:15:13 +0900

    ✨ feat: 기본 프로필 이미지 추가

--- src/components/productdetail/SellerInfo.jsx
+++ src/components/productdetail/SellerInfo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { PRODUCT_DEFAULT_IMG } from '../../constants/mypageConstants';
 
 const SellerInfo = ({ seller }) => {
   return (
     <div className="flex items-center gap-[20px] px-[20px] lg:px-[30px] py-[20px] border-b-[1px] border-light-gray">
       <img
-        src={seller?.profile_img}
+        src={seller?.profile_img || PRODUCT_DEFAULT_IMG}
         alt={seller?.name}
         className="w-10 h-10 bg-gray-300 rounded-full"
       />
